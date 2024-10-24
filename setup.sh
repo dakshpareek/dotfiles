965ee6a6bb15e88667e5cddee97ecdc7510cab97
Daksh Personal <dakshpareek7@gmail.com>
2024-10-24 22:43:22 +0530

    feat: add alacritty config file and ssh config file

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Create symlink for Alacritty config
+ln -s ~/personal-projects/dotfiles/alacritty.toml ~/.config/alacritty/alacritty.toml
+
+
+# How to run
+# chmod +x setup.sh  # Make the script executable
+# ./setup.sh
