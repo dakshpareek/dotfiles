b24692f05a1e735ae7537884160885c392acc32a
Daksh Personal <dakshpareek7@gmail.com>
2024-10-25 15:58:39 +0530

    feat: add alacritty config

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,4 +5,8 @@ alias ns="npm run start"
 alias nsd="npm run start:dev"
 alias test="npm run test"
 alias lg="lazygit"
-alias gp="git push origin $(git branch --show-current)"
+
+unalias gp 2>/dev/null  # Ignore any error if alias doesn't exist
+gp() {
+  git push origin "$(git branch --show-current)"
+}
